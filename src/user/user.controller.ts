cf3adadb565403768deb419967e15a8aef6d9dcf
jegonovakosk <62294366+jegonovakosk@users.noreply.github.com>
2023-08-08 18:19:59 -0300

    validação do do post usuarios

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,19 +1,29 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from './DTO/createUser.dto';
 import { retry } from 'rxjs';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
-
+import { returnUserDto } from './DTO/returnUser.dto';
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
+  @UsePipes(ValidationPipe)
   @Post()
   async CreateUser(@Body() createUserDto: CreateUserDto): Promise<UserEntity> {
     return await this.userService.createUser(createUserDto);
   }
 
   @Get()
-  async getAllUser(): Promise<UserEntity[]> {
-    return this.userService.getAllUser();
+  async getAllUser(): Promise<returnUserDto[]> {
+    return (await this.userService.getAllUser()).map(
+      (userEntity) => new returnUserDto(userEntity),
+    );
   }
 }
